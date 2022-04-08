48697ba5955867f762652529b95038939ae65a4b
shakoor123 <shakoorchachu2001@gmail.com>
2022-04-08 15:51:59 +0530

    first commit collage project

---
+++ routes/admin.js
@@ -0,0 +1,200 @@
+var express = require('express');
+const async = require('hbs/lib/async');
+var router = express.Router();
+var app = express();
+const path = require('path');
+//database connection
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'mysql',
+  database: 'project'
+})
+connection.connect(function (err) {
+  if (err) throw err
+  console.log('database connected...')
+
+})
+//image uploading hedder
+const multer=require('multer');
+const storage=multer.diskStorage({
+  destination:'./public/images',
+  filename:(req,file,cb)=>{
+    return cb(null,`pic_${Date.now()}${path.extname(file.originalname)}`)
+  }
+})
+
+const upload=multer({
+  storage:storage
+});
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('admin/loginA');
+});
+//home page all details of students
+router.get('/home',(req,res)=>{
+  var sql= `select * from image;`
+  connection.query(sql,(err,result)=>{
+    if(err) throw err;
+    else{
+      res.render('admin/home',{result})
+    }
+  })
+  
+})
+//notification  page
+router.get('/notification',(req,res)=>{
+  connection.query(`SELECT * FROM notification `, function (err, result, fields) {
+    if (err) throw err;
+    res.render('admin/notification',{result})
+
+  });
+})
+
+//Addnotification  page
+router.get('/addnotification',(req,res)=>{
+  res.render('admin/addNotification')
+})
+//add notification operation
+router.post('/addnotification',async(req,res)=>{
+  console.log(req.body);
+  var sql = `INSERT INTO notification VALUES ("${req.body.title}","${req.body.About}","${req.body.cgpa}")`;
+  await connection.query(sql,async function (err, result) {
+    if (err) throw err;
+    else{
+      var title=req.body.title;
+      var sql=`create table ${title}(name varchar(200),phone bigint,branch varchar(100),rno bigint,cgpa float);`
+      await connection.query(sql,(err,result)=>{
+        if (err) throw err;
+        else{
+          res.redirect('/admin/notification')
+        }
+      })
+    }
+  })
+})
+
+//admin login operation
+router.post('/loginA',(req,res)=>{
+  res.redirect('/admin/home')
+})
+//delete notification
+router.get('/deleteNotification/:name',async(req,res)=>{
+  let name=req.params.name;
+  var sql = `delete from notification where title="${name}"`;
+  await connection.query(sql,async function (err, result) {
+    if (err) throw err;
+    else{
+      var sql=`drop table ${name};`
+      await connection.query(sql,(err,result)=>{
+        if(err) throw err;
+        else{
+          res.redirect('/admin/notification')
+        }
+      })
+    }
+  })
+})
+//student details page
+router.get('/students',(req,res)=>{
+  res.render('admin/students')
+})
+//all students details
+router.get('/allstudents',async(req,res)=>{
+  var sql=`select * from student`;deleteimg/pic_1649
+  await connection.query(sql,(err,result)=>{
+    if(err) throw err;
+    else{
+      res.render('admin/allstudents',{result})
+    }
+  })
+})
+//select students branch wise
+router.post('/branch',async(req,res)=>{
+  console.log(req.body);
+  let branch=req.body.selected;
+  var sql=`select * from student where branch="${branch}";`
+  await connection.query(sql,(err,result)=>{
+    if (err) throw err;
+    else{
+      res.render('/admin/branchstudents',{result})
+    }
+  })
+})
+//delete student
+router.get('/deletestudent/:ph',async(req,res)=>{
+  phone=req.params.ph;
+  console.log(phone);
+var sql=`DELETE FROM student WHERE phone="${phone}";`
+await connection.query(sql,(err,result)=>{
+  if(err) throw err;
+  else{
+    res.redirect('/admin/allstudents')
+  }
+})
+
+})
+//notification applayed students
+router.get('/notificationApplyed/:title',async(req,res)=>{
+  res.send(req.params.title)
+  var title=req.params.title;
+  var sql=`select * from ${title};`
+  await connection.query(sql,(err,result)=>{
+    if(err) throw err;
+    else{
+      res.render('admin/applayed',{result,title})
+    }
+  })
+})
+//selecting single student for edit details
+router.post('/search',(req,res)=>{
+  var sql=`select * from student where phone=${req.body.search};`
+  connection.query(sql,(err,result)=>{
+    if(err) throw err;
+    else{
+      console.log(result);
+      result=result[0];
+      res.render('admin/singlestudent',{result})
+    }
+  })
+})
+//change student details
+router.post('/changeDetails',(req,res)=>{
+  console.log(req.body);
+  var sql=`UPDATE student SET rno="${req.body.regno}",name="${req.body.name}",email="${req.body.email}",cgpa="${req.body.cgpa} WHERE phone="${req.body.phone}";`
+  connection.query(sql,(err,result)=>{
+    if(err) throw err;
+    else{
+      res.redirect('/search');
+    }
+  })
+})
+
+
+
+
+
+
+app.use('/profile',express.static('public/images'));
+// inserting the image
+router.post('/insertimage',upload.single('pic'),async(req,res)=>{
+
+  var sql=`insert into image values("${req.file.filename}");`
+  await connection.query(sql,(err,result)=>{
+    if(err) throw err;
+    else{
+    res.redirect("/admin/home");
+     
+    }
+  })
+})
+//delete image of front page
+router.get('/deleteimg/:img',(req,res)=>{
+  var image=req.params.img;
+  var sql=`DELETE FROM image WHERE img="${image}";`
+  connection.query(sql,(err,result)=>{
+    res.redirect('/admin/home');
+  })
+})
+module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,156 @@
+var express = require('express');
+const async = require('hbs/lib/async');
+var router = express.Router();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+//database connection
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'mysql',
+  database: 'project'
+})
+connection.connect(function (err) {
+  if (err) throw err
+  console.log('database connected...')
+
+})
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var user=req.session.user;
+  var image;
+  var sql= `select * from image;`
+  connection.query(sql,(err,result)=>{
+    if(err) throw err;
+    else{
+      image=result;
+    }
+  })
+  var sql=`select * from notification`;
+  connection.query(sql,(err,result)=>{
+    if(err) throw err;
+    else{
+     
+      res.render('index',{user,result,image});
+    }
+  })
+  
+});
+//sign up page getting
+router.get('/signup',(req,res)=>{
+  if(req.session.signup){
+    sign=req.session.signup;
+    res.render('signup',{sign});
+  }else{
+    res.render('signup');
+  }
+})
+//sign up operation
+router.post('/signup',async(req,res)=>{
+  var sql=`select COUNT(*) AS c from student`;
+  await connection.query(sql,async function(err,result){
+    if(err) throw err;
+    else{
+      //console.log(result[0].c);
+      let s=result[0].c;
+      var sql=`select phone from student`;
+      await connection.query(sql,(err,result)=>{
+        if(err) throw err;
+        else{
+          let com=0;
+          for(let i=0;i<s;i++){
+            if(result[i].phone==req.body.mobile){
+               com++;
+            }
+          }
+          if(com!=0){
+            req.session.signup=true;
+            res.redirect('/signup')
+          }else{
+            com=0;
+            bcrypt.hash(req.body.password, saltRounds,async function(err, hash) {
+              var sql = `INSERT INTO student VALUES ("${req.body.regno}","${req.body.name}","${req.body.email}","${req.body.mobile}","${req.body.cgpa}","${hash}")`;
+            await connection.query(sql, function (err, result) {
+              if (err) throw err;
+              else{
+                req.session.signup=false;
+                res.redirect('/login')
+              }
+            })
+          });
+          }
+        }
+      })
+    }
+  })
+
+})
+//login user page
+router.get('/login',(req,res)=>{
+  if(req.session.user){
+    res.redirect('/')
+  }else{
+    var loginFail=req.session.Fail;
+    res.render('login',{loginFail})
+  }
+})
+//login operation
+router.post('/login',async(req,res)=>{
+  var sql=`select COUNT(*) AS c from student`;
+  await connection.query(sql,async function(err,result){
+    if(err) throw err;
+    else{
+      //console.log(result[0].c);
+      let s=result[0].c;
+      var sql=`select phone from student`;
+      await connection.query(sql,(err,result)=>{
+        if(err) throw err;
+        else{
+          let com=0;
+          for(let i=0;i<s;i++){
+            if(result[i].phone==req.body.mobile){
+               com++;
+            }
+          }
+          if(com!=0){
+
+            var sql = `select * from student where phone=${req.body.mobile}`;
+             connection.query(sql, function (err, result) {
+             if (err) throw err;
+             else{
+             bcrypt.compare(req.body.password,result[0].password, function(err, status) {
+             if(status){
+                req.session.user=result[0];
+                 req.session.loggin=true;
+                res.redirect('/')
+              }else{
+                req.session.Fail=true;
+                res.redirect('/login')
+               }
+              });
+            }
+          })
+          }else{
+            com=0;
+            req.session.Fail=true;
+             res.redirect('/login')
+        
+          }
+        }
+      })
+    }
+  })
+})
+//logout operation
+router.get('/logout',(req,res)=>{
+  req.session.destroy();
+  res.redirect('/login')
+})
+//cgpa kk poovaan
+router.get('/cgpa',(req,res)=>{
+  user=req.session.user;
+  res.render('cgpa',{user})
+})
+
+module.exports = router;
